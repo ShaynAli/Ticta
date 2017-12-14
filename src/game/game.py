8d1a079d938480e4b4fafd6556cd2a6878e365fa
ShaynAli <shayaan.syed.ali@gmail.com>
2017-12-14 18:54:30 -0500

    Tictactoe 2.0

--- src/game/game.py
+++ src/game/game.py
@@ -11,7 +11,7 @@ class Player:
     def __init__(self):
         pass
 
-    def prompt(self, first_prompt, condition, second_prompt=None):
+    def prompt(self, first_prompt, condition=None, second_prompt=None):
         '''
         Prompts the user until they respond with an input that meets some condition
         :param first_prompt: A prompt for the user to enter information after
@@ -21,10 +21,12 @@ class Player:
         :return:
         '''
         user_input = input(first_prompt)
-        if condition(user_input):
-            return user_input
-        else:  # Prompt again
+        if condition is not None:
+            if condition(user_input):
+                return user_input
+            # Prompt again
             if second_prompt is None:
                 return self.prompt(first_prompt, condition)
-            else:
-                return self.prompt(second_prompt, condition)
+            return self.prompt(second_prompt, condition)
+        # Without a condition the user input is always returned
+        return user_input
