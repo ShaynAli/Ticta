404be0473d3803cae22c2469011ec6e664f181c1
Davinderpal Cheema <dcheema6@uwo.ca>
2017-12-13 19:57:53 -0500

    turn logic

---
+++ src/client/Presentation.py
@@ -0,0 +1,87 @@
+from tkinter import *
+from tkinter.font import Font
+from threading import Thread
+import sys
+
+
+def start_game(event):
+    event.widget.pack_forget()
+
+    for x in range(0, game_rows):
+        for y in range(0, game_columns):
+            game_array[x][y].config(state=NORMAL, text=" ", bg="white")
+    top_label.config(text="Looking for a player")
+
+    #call client to get start game:
+    #   get label and get is_turn value
+
+    top_label.config(text="Game begins...")
+
+    #wait for response
+
+
+def quit_game(event):
+    sys.exit()
+
+
+def button_press(event):
+
+    #send data to server
+
+    if event.widget["state"] == NORMAL:
+        event.widget.config(text=my_label, disabledforeground=my_color, bg="black", state=DISABLED)
+
+
+#variables
+is_turn = True
+
+game_rows = 3
+game_columns = 3
+
+game_array = [[0 for x in range(game_rows)] for y in range(game_columns)]
+
+my_label, my_color = "X", "red"
+
+opponent_label, opponent_color = "O", "blue"
+
+
+#main window
+window = Tk()
+window.geometry("452x540")
+
+#GUI frame skeleton
+top_frame = Frame(window)
+top_frame.pack(fill=BOTH, side=TOP)
+
+mid_frame = Frame(window)
+mid_frame.pack(fill=BOTH)
+
+bottom_frame = Canvas(window)
+bottom_frame.pack(fill=X, side=BOTTOM)
+
+#GUI widgets
+
+top_label = Label(top_frame, text="The SEXY Tic Tak Toe...", bg="black", fg="white")
+top_label.pack(fill=X)
+
+my_font = Font(family="Helvetica", size=60)
+
+grid_canvas = Canvas(mid_frame)
+grid_canvas.place()
+
+for x in range(0, game_rows):
+    for y in range(0, game_columns):
+        button = Button(mid_frame, width=3, height=0, state=DISABLED, font=my_font, bg="white", disabledforeground="white", text=str(x)+str(y))
+        button.grid(row=x, column=y, sticky=NSEW)
+        button.bind('<Button-1>', button_press)
+        game_array[x][y] = button
+
+button_start = Button(bottom_frame, text="New game", fg="green")
+button_start.bind("<Button-1>", start_game)
+button_start.pack(fill=X)
+
+button_quit = Button(bottom_frame, text="Quit/Close", fg="red")
+button_quit.bind("<Button-1>", quit_game)
+button_quit.pack(fill=X)
+
+window.mainloop()
--- src/client/client.py
+++ src/client/client.py
@@ -11,8 +11,13 @@ gameEnd = False
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
 
+<<<<<<< HEAD
 data = client.recv(BUFFER_SIZE)
 print(data.decode())
+=======
+    def receive_from_server(self):
+        return self.client_socket.recv(1024).decode()
+>>>>>>> 88ca249... modified layout
 
 if data.decode() == 'Waiting for player to match':
     data = client.recv(BUFFER_SIZE)
@@ -25,17 +30,80 @@ while data != 'end':
         MESSAGE = input("Enter your move or end:")
         client.send(MESSAGE.encode())
 
+<<<<<<< HEAD
     #game logic
+=======
+host = '127.0.0.1'
+port = 12000
+>>>>>>> 9432c02... gui client w/o logic
 
     if not gameEnd:
         print('Wait for opponent move')
 
+<<<<<<< HEAD
     if MESSAGE == 'end':
         break
 
     data = client.recv(BUFFER_SIZE)
-    data = client.recv(BUFFER_SIZE)
     print("Opponent move:", data.decode())
+    data = client.recv(BUFFER_SIZE)
 
 
 client.close()
+=======
+# BUFFER_SIZE = 1024
+# MESSAGE = " "
+# def send_move_server(server_socket, column_number, row_number):
+#     message = 'move' + ' ' + str(column_number) + ' ' + str(row_number)
+#     server_socket.send(message.encode())
+#
+# def opponent_move():
+#     pass
+#
+# def on_win():
+#     pass
+#
+# def on_draw():
+#     pass
+#
+#
+# gameEnd = False
+#
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.connect((host, port))
+#
+# data = server.recv(BUFFER_SIZE)
+# print(data.decode())
+#
+# if data.decode() == 'Waiting for player to match':
+#     data = server.recv(BUFFER_SIZE)
+#     print(data.decode())
+#
+# data = data.decode()
+# while data != 'end':
+#
+#     if data == 'Turn':
+#         # will need to be user interface but console works for testing
+#         column = input("Enter your column:")
+#         row = input ('Enter your row:')
+#         send_move_server(server, column, row)
+#     if data == 'wait':
+#         data = server.recv(BUFFER_SIZE)
+#         if data == 'move':
+#             opponent_move()
+#     if data == 'win':
+#         on_win()
+#     if data == 'draw':
+#         on_draw()
+#
+#
+#
+#
+#     data = client.recv(BUFFER_SIZE)
+#     print("Opponent move:", data.decode())
+#     data = client.recv(BUFFER_SIZE)
+#
+#
+#
+# client.close()
+>>>>>>> 9432c02... gui client w/o logic
