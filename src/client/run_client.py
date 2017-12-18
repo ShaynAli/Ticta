3a4d902fad448dcd621531487b7e2171101dfbdf
ShaynAli <shayaan.syed.ali@gmail.com>
2017-12-18 03:38:36 -0500

    Action system complete, repo refactoring

--- src/client/run_client.py
+++
@@ -1,6 +0,0 @@
-import socket
-from client import Client
-
-client = Client()
-client.connect((socket.gethostname(), 12000))
-client.play()
--- src/server/run_server.py
+++
@@ -1,6 +0,0 @@
-import socket
-from server import Server
-# from common.constants import SERVER_PORT
-
-server = Server(12000)
-server.listen()
